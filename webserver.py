648308c60f8922e9f152b9febf13ad142af6638c
Keith <keith.short32@gmail.com>
2022-09-30 14:18:58 +0000

    added a hello world file

--- webserver.py
+++ webserver.py
@@ -3,7 +3,8 @@ from http.server import HTTPServer, CGIHTTPRequestHandler
 
 os.chdir('.')
 
-server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+
+server_object = HTTPServer(server_address=('', 8080), RequestHandlerClass=CGIHTTPRequestHandler)
 
 server_object.serve_forever()
 print("Server is running")
